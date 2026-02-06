a2b493b82424b34af7338083caaf3bd19de36619
chrisnew143-gif <chrisnew143@gmail.com>
2026-02-06 10:13:41 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,14 @@
 import streamlit as st
 
 st.set_page_config(page_title="Pickleball Manager", layout="centered")
-
 st.title("🎾 Pickleball Stack System")
 
-st.write("Choose a mode:")
+st.write("Welcome! Choose a mode:")
 
 col1, col2 = st.columns(2)
 
 if col1.button("🏟 Organizer (AutoStack)", use_container_width=True):
-    st.switch_page("pages/1_🏟_AutoStack.py")
+    st.switch_page("1_🏟_AutoStack")
 
 if col2.button("👤 Player Join", use_container_width=True):
-    st.switch_page("pages/2_👤_Player.py")
+    st.switch_page("2_👤_Player")
